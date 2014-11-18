9f28df40c25ffdc4ae2c072521983bf04969b4fa
Pete Petrash <pete.petrash@gmail.com>
2014-11-17 21:28:59 -0800

    add forecast apikey and base/forecast urls

--- Stormy/ViewController.swift
+++ Stormy/ViewController.swift
@@ -9,10 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // set Forecast.io apikey constant
+    private let apiKey = "8c4ca462befc88e8371c66495a8057a5"
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let baseURL = NSURL(string: "https://api.forecast.io/forecast/\(apiKey)/")
+        let forecastURL = NSURL(string: "37.576876, -122.267081", relativeToURL: baseURL)
     }
 
     override func didReceiveMemoryWarning() {
